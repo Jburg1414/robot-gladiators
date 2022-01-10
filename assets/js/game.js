6ce5ef4da703df2a31b923a9cbfda07dafa4366e
Jburg1414 <Jburgess1414@gmail.com>
2022-01-09 21:23:14 -0700

    fixed fight/skip prompt

--- assets/js/game.js
+++ assets/js/game.js
@@ -4,28 +4,40 @@ var randomNumber = function (min, max) {
   return value;
 };
 
+var fightOrSkip = function() {
+    // ask player if they'd like to fight or skip using fightOrSkip function
+    var promptFight = window.prompt("Would you like to FIGHT for SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
+
+    if (promptFight === "" || promptFight === null) {
+        window.alert("You need to provide a valid answer! Please try again.");
+        return fightOrSkip();
+    }
+
+    promptFight = promptFight.toLowerCase();
+
+    // if player picks 'skip' confirm and then stop the loop
+    if (promptFight === "skip") {
+        // confirm player wants to skip
+        var confirmSkip = window.confirm("Are you sure you'd like to quit?");
+
+        // if yes (true), leave fight
+        if (confirmSkip) {
+            window.alert(playerInfo.name + " has decided to skip this fight. Goodbye!");
+            // subtract money from playerMoney for skipping
+            playerInfo.money = Math.max(0, playerInfo.money - 10);
+
+            return true;
+        }
+    }
+
+    return false;
+}
+
 var fight = function (enemy) {
   while (playerInfo.health > 0 && enemy.health > 0) {
-    //Choose to fight or skip battle.
-    var prompFight = window.prompt(
-      "Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose."
-    );
+    if (fightOrSkip());
+    break;
 
-    //if player picks "skip" confirm and then stop loop
-    if (prompFight === "skip" || prompFight === "SKIP") {
-      //confirm player wants to skip
-      var confirmSkip = window.confirm("Are you sure you'd like to quit?");
-      //if yes (true), leave fight
-      if (confirmSkip) {
-        window.alert(
-          playerInfo.name + " has decided to skip this fight. Goodbye!"
-        );
-        //subtract money from playerInfo.money for skipping
-        playerInfo.money = Math.max(0, playerInfo.money - 10);
-        console.log("playerInfo.money", playerInfo.money);
-        break;
-      }
-    }
     // Remove enemy's health by subtracting the amount set in playerInfo.attack variable
     var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
 
@@ -94,8 +106,6 @@ var startGame = function () {
 
       // reset enemy.health before starting new fight
       pickedEnemyObj.health = randomNumber(40, 60);
-
-      //debugger
 
       //pass the pickedEnemyName variable's value into the fight function, which it will assume the value of the enemy.name parameter
       fight(pickedEnemyObj);
